ab8c0c3c42aa0b0a383aad585edc26b0fc016676
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-02-18 10:47:57 +0000

    update nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -119,9 +119,18 @@ const Navbar = () => {
         </nav>
         <Link
           to="/"
-          className="flex items-center justify-center w-60 h-20 m-2 text-blue-900 text-xl font-medium border-2 border-blue-900 rounded-3xl hover:bg-blue-800 hover:text-white transition"
+          className="flex items-center justify-center w-60 h-20 m-2 text-blue-900 text-xl font-medium border-2 border-yellow-400 rounded-3xl hover:bg-blue-800 hover:text-white transition"
         >
-          Call : 07123-456789
+          <svg
+            class="w-8 h-8 text-blue-600 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 19 18"
+          >
+            <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z" />
+          </svg>
+          : 07123-456789
         </Link>
       </div>
     </header>
