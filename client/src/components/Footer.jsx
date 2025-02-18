84939672ea7d30ddca7ca07c8ea3196a801aa2a1
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-02-18 12:18:31 +0000

    update footer grid

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-blue-700 text-gray-100 py-12">
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
           {/* Company Info */}
           <div>
             <h2 className="text-xl font-semibold">Care Home</h2>
