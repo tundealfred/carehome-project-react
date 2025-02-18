f6d6a37711e932da4d8ecefcb54ce53b17d5e25c
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-02-18 00:17:12 +0000

    edit home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import HeroSection from "../components/HeroSection";
 import carePhoto1 from "../assets/carePhoto1.jpg";
 import carePhoto2 from "../assets/carePhoto2.jpg";
+import carePhoto7 from "../assets/carePhoto7.jpg";
 
 const Home = () => {
   return (
@@ -18,11 +19,15 @@ const Home = () => {
             whileHover={{ scale: 1.05 }}
           >
             <h3 className="text-xl font-bold mb-2">Professional Staff</h3>
-            <p>
+            <p className="py-2">
               Our team is experienced and dedicated to providing exceptional
               care.
             </p>
-            <img src={carePhoto1} alt="Pro staff" />
+            <img
+              src={carePhoto1}
+              alt="Pro staff"
+              className="rounded-lg shadow-lg object-cover"
+            />
           </motion.div>
           <motion.div
             className="bg-white shadow-lg p-6 rounded-lg text-center"
@@ -31,16 +36,30 @@ const Home = () => {
             <h3 className="text-xl font-bold mb-2">
               State-of-the-Art Facilities
             </h3>
-            <p>Modern amenities designed for comfort and safety.</p>
-            <img src={carePhoto2} alt="facility" />
+            <p className="py-2">
+              Modern amenities designed for comfort and safety that meets health
+              and regulatory standards.
+            </p>
+            <img
+              src={carePhoto7}
+              alt="facility"
+              className="rounded-lg shadow-lg object-cover"
+            />
           </motion.div>
           <motion.div
             className="bg-white shadow-lg p-6 rounded-lg text-center"
             whileHover={{ scale: 1.05 }}
           >
             <h3 className="text-xl font-bold mb-2">Personalized Care</h3>
-            <p>Customized plans to meet individual needs and preferences.</p>
-            <img src={carePhoto2} alt="care" />
+            <p className="py-2">
+              Customized plans to meet individual needs and preferences for our
+              members.
+            </p>
+            <img
+              src={carePhoto2}
+              alt="care"
+              className="rounded-lg shadow-lg object-cover"
+            />
           </motion.div>
         </div>
       </div>
