983018181b14f055652150cc2660dd4d34f15af3
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-02-25 11:59:25 +0000

    fix faq z

--- client/src/pages/FAQs.jsx
+++ client/src/pages/FAQs.jsx
@@ -54,7 +54,7 @@ const FAQs = () => {
       >
         <div className="bg-black bg-opacity-50 absolute inset-0"></div>
         <motion.div
-          className="relative z-10 px-6"
+          className="relative px-6"
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
